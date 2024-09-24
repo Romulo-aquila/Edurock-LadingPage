f912eecdcf2802f7a87136483c9ec96b301088d0
romuloaquila <romuloaquila21@gmail.com>
2024-09-24 19:55:51 -0300

    Configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Edurock-LadingPage",
+  base: "/Edurock-LadingPage",
   publicDir: 'public',
   build: {
     outDir: 'dist',
