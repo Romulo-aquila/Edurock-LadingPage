3b53e54941f5000a83333b6cd1fe62fecde57f83
romuloaquila <romuloaquila21@gmail.com>
2024-10-02 16:31:34 -0300

    Finalizando Formulario

--- src/components/RegistrationSession.jsx
+++ src/components/RegistrationSession.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { FaPlay } from "react-icons/fa";
+import { MdArrowRightAlt } from "react-icons/md";
 
 function RegistrationSession() {
     const Container = styled.div`
@@ -83,10 +84,115 @@ function RegistrationSession() {
     const RegisterField = styled.div`
         width: 470px;
         height: 575.4px;
-        background-color: #fff;
+        background-color: #f5f5fe;
+        border-radius: 5px;
     `;
 
-    const RgText = styled.h2``;
+    const FormField = styled.form`
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+        padding: 0 2.4rem;
+    `;
+
+    const InputName = styled.input`
+        outline: none;
+        border: none;
+        padding-left: 12px;
+        background-color: #f0f1f3;
+        font-size: 16.5px;
+        width: 400px;
+        height: 48px;
+    `;
+
+    const InputEmail = styled.input`
+        outline: none;
+        border: none;
+        padding-left: 12px;
+        background-color: #f0f1f3;
+        font-size: 16.5px;
+        width: 185px;
+        height: 48px;
+    `;
+
+    const InputPhone = styled.input`
+        outline: none;
+        border: none;
+        padding-left: 12px;
+        background-color: #f0f1f3;
+        font-size: 16.5px;
+        width: 200px;
+        height: 48px;
+        `;
+
+    const InputAddress = styled.input`
+        outline: none;
+        border: none;
+        padding-left: 12px;
+        background-color: #f0f1f3;
+        font-size: 16.5px;
+         width: 400px;
+         height: 48px;
+    `;
+
+    const TextArea = styled.textarea`
+        border: none;
+        padding-left: 2.5px;
+        background-color: #f0f1f3;
+        font-size: 16.5px;
+        width: 400px;
+        height: 155px;
+
+        &::placeholder {
+            color: #000;
+            opacity: 100%;
+        }
+    `;
+
+    const EmailAndPhone = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 10px;
+    `;
+
+
+    const InputBtn = styled.button`
+        cursor: pointer;
+        font-size: 14.5px;
+        font-weight: 400;
+        color: #fff;
+        border: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #5f2ded;
+        border-radius: 4px;
+        width: 130px;
+        height: 48px;
+        transition: 0.2s;
+
+        &:hover {
+            background-color: #fff;
+            border: 1px solid #5f2ded;
+            color: #5f2ded;
+        }
+    `;
+
+    const IconBtn = styled.span`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-left: 5px;
+        font-size: 12pt;
+    `;
+    
+
+    const FormTitle = styled.h3`
+        font-size: 20px;
+        padding-top: 2rem;
+    `;
+
 
     const TextSection = styled.div`
         width: 65%;
@@ -105,7 +211,7 @@ function RegistrationSession() {
     <>
     <Container>
      <TextSection>
-        <CaptionTxt>Registre-se</CaptionTxt>
+        <CaptionTxt>Registrar</CaptionTxt>
         <Title>Registre Sua <Underline>Conta</Underline> e Obtenha Acesso Gratuito a 
             <NumYellow> 60000</NumYellow> cursos online </Title>
         <PlayAndSubTitle>
@@ -115,7 +221,17 @@ function RegistrationSession() {
         </PlayAndSubTitle>
     </TextSection>
         <RegisterField>
-            <RgText>Registre-se</RgText>
+            <FormField>
+                <FormTitle>Registre sua Conta</FormTitle>
+        <InputName type="text" placeholder="Seu Nome" /> 
+        <EmailAndPhone>
+            <InputEmail type="text" placeholder="Email" />
+            <InputPhone type="text" placeholder="Celular" />
+        </EmailAndPhone>
+        <InputAddress type="text" placeholder="Endereço" />
+        <TextArea placeholder="Comentário" />
+        <InputBtn type="button" >Cadastrar-se <IconBtn><MdArrowRightAlt/></IconBtn></InputBtn>
+            </FormField>
         </RegisterField>
     </Container>
     <WSpace></WSpace>
